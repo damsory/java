a99ef94bbac3230dde83649ccc43812ba819af80
damsory <damsora@naver.com>
2023-12-08 17:14:15 +0900

    첫 자바

--- java_dss/src/day01/HelloWorld.java
+++ java_dss/src/day01/HelloWorld.java
@@ -1,11 +1,21 @@
+//주석
+/*
+ * 주석입니다.
+ * */
 package day01;
 
-public class HelloWorld {
+public class HelloWorld { //주석
 
+	//주석
+	/**
+	 * 메소드를 설명할 때 사용하는 주석
+	 * @param args
+	 */
 	public static void main(String[] args) {
+		//sysout은 콘솔에 출력할 때 사용
 		// TODO Auto-generated method stub
-		
 		System.out.println("Hello World!");
+		
 	}
 
 }
