7c7beb76f4a0c4627a0cbd10e4a4391a5d01dba1
damsory <damsora@naver.com>
2023-12-08 17:44:18 +0900

    집에서 연습 중

--- java_dss/src/day01/TestEx1.java
+++ java_dss/src/day01/TestEx1.java
@@ -3,10 +3,10 @@ package day01;
 public class TestEx1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//문제1.
+		//char ch1 ="A";
 		
 		char ch1 ='A';
-		System.out.println(ch1);
 	}
 
 }
--- java_dss/src/day01/TestEx2.java
+++ java_dss/src/day01/TestEx2.java
@@ -3,9 +3,11 @@ package day01;
 public class TestEx2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int num1 = 70;
-		
+		// 문제2. 수학성적 평균 구하는데 필요한 변수 선언
+		// 국어, 영어, 수학 점수를 어떤 값을 이용할 것인가? 점수를 정수로 할 것인가? 실수로 할 것인가?
+		int kor1, eng1,math1,sum1;
+		double kor2,eng2,math2, sum2;
+		double avg;
 	}
 
 }
