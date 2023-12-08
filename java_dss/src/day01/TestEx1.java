fd4933d4f9ce6d50953d3aa45753086a52d7ac7a
damsory <damsora@naver.com>
2023-12-08 17:42:03 +0900

    자바 1일차 연습문제

---
+++ java_dss/src/day01/TestEx1.java
@@ -0,0 +1,12 @@
+package day01;
+
+public class TestEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		char ch1 ='A';
+		System.out.println(ch1);
+	}
+
+}
