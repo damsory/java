25e0813b3402b066c917b4c0a2df3403c35953b0
damsory <damsora@naver.com>
2023-12-11 17:34:23 +0900

    자바 2일차:if else if문 예제

---
+++ java_dss/src/IfelseifEx1.java
@@ -0,0 +1,21 @@
+
+public class IfelseifEx1 {
+
+	// if else if문 예제
+	public static void main(String[] args) {
+		
+		int num = -32;
+		
+		//num가 0이면 0이라고 출력하고, num가 양수이면 양수라고 출력하고, num가 음수이면 음수라고 출력하는
+		//코드를 작성하세요.
+		
+		if(num == 0) {
+			System.out.println(num);
+		}else if(num > 0){
+			System.out.println(num +"는 양수");
+		} else {
+			System.out.println(num +"는 음수");
+		}
+	}
+
+}
