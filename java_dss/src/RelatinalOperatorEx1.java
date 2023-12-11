6bcfd1f04e6f2c3035e0aa74cb8594d11ad8ff10
damsory <damsora@naver.com>
2023-12-11 12:46:24 +0900

    자바 2일차:비교 (관계) 연산자 예제

---
+++ java_dss/src/RelatinalOperatorEx1.java
@@ -0,0 +1,17 @@
+
+public class RelatinalOperatorEx1 {
+
+	// 비교 (관계)연산자 예제
+	public static void main(String[] args) {
+		int num1 =1, num2 = 2;
+		
+		System.out.println(num1+ ">" + num2 + " : "  +(num1 > num2));
+		System.out.println(num1+ ">=" + num2 + " : " +(num1 >= num2));
+		System.out.println(num1+ "<" + num2 + " : "  +(num1 < num2));
+		System.out.println(num1+ "<=" + num2 + " : " +(num1 <= num2));
+		System.out.println(num1+ "==" + num2 + " : " +(num1 == num2));
+		System.out.println(num1+ "!=" + num2 + " : " +(num1 != num2));
+		
+	}
+
+}
