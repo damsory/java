33dc7b367db56c80a43fb8e34c1b8fef73f99f66
damsory <damsora@naver.com>
2023-12-15 12:50:22 +0900

    자바 6일차 배열 꽉참

--- java_dss/src/day06/ArrayExpandEx1.java
+++ java_dss/src/day06/ArrayExpandEx1.java
@@ -5,17 +5,26 @@ public class ArrayExpandEx1 {
 	public static void main(String[] args) {
 		// 배열이 꽉 찼을 때 배열을 확장하는 예제
 		
-		int arr[]=new int[5];
 		
+		//배열 5개의 arr을 저장하는 배열
+		int arr[]=new int[5];
+		//count 초기화;
 		int count =10;
+		//i가 count보다 작으면 1증가
 		for(int i=0;i<count;i++) {
+			//만약 i+1이 arr보다 작으면 
 			if(i+1>arr.length) {
+				//tmp 배열 선언 후 arr에서 부족한 배열 확장
 				int tmp[]=new int[arr.length+5];
+				//arr배열에 저장된것 복사
 				System.arraycopy(arr, 0, tmp, 0, arr.length);
+				//arr가tmp로 이동
 				arr= tmp;
 			}
+			//다시 배열 저장
 			arr[i]=i+1;
 		}
+		//
 		for(int tmp : arr) {
 			System.out.println(tmp);
 		}
