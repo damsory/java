5816c638eac48596e23744b3f2342c60f25ae2d1
damsory <damsora@naver.com>
2024-01-09 15:34:35 +0900

    수정

--- java_dss/src/sksksk.java
+++ java_dss/src/sksksk.java
@@ -1,32 +1,19 @@
-import java.util.ArrayList;
-
-import lombok.EqualsAndHashCode;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class sksksk {
 	
 	public static void main(String[] args) {
-		ArrayList<Fruit> list =new ArrayList<Fruit>();
-		list.add(new Fruit("사과","빨강"));
-		list.add(new Fruit("메론","초록"));
-		list.add(new Fruit("포도","보라"));
-		list.add("맛있는 과일");
-		
-		for(int i=0; i<=list.size();i++) {
-			System.out.println(list.get(i));
+		 
+			FileWriter fw =null;
+			try {
+				
+				fw=new FileWriter("test.txt");
+				fw.write(97);
+				fw.write(65);
+			}catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
-	}
-@EqualsAndHashCode
-class Fruit{
-private String name;
-private String color;	
-	public void print() {
-		System.out.println(name);
-	}
-	
-	public Fruit(String name , String color) {
-		this.name =name;
-		this.color=color;
-		
-	}
 }
-}
+
